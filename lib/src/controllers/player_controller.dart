1233051292ec22de1777ac60a6a0aa77d1878ef7
hoangdosafety <77949614+hoangdosafety@users.noreply.github.com>
2023-06-06 17:00:53 +0700

    Update player_controller.dart

--- lib/src/controllers/player_controller.dart
+++ lib/src/controllers/player_controller.dart
@@ -231,11 +231,12 @@ class PlayerController extends ChangeNotifier {
   /// otherwise nothing happens.
   Future<void> seekTo(int progress) async {
     if (progress < 0) return;
-    if (_playerState == PlayerState.playing) {
+    if (_playerState == PlayerState.playing ||
+        _playerState == PlayerState.paused) {
       await AudioWaveformsInterface.instance.seekTo(playerKey, progress);
     }
   }
-
+  
   /// Release any resources taken by this controller. Disposing this
   /// will stop the player and release resources from native.
   ///
